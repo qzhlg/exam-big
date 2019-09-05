d8f84185f09bb883620558d6161d10b4cc8adeee
qzhlg <zhl042768g@163.com>
2019-09-05 20:38:55 +0800

    添加试题数据

--- src/views/home/text/addtext.tsx
+++ src/views/home/text/addtext.tsx
@@ -1,5 +1,47 @@
 import * as React from 'react';
-class Addtext extends React.Component {
+import { inject, observer } from 'mobx-react'
+import { Button, Select } from 'antd'
+const { Option } = Select;
+
+interface Props {
+
+  subject: any,
+
+  question: any
+}
+@inject('subject', 'question')
+@observer
+
+class Addtext extends React.Component<Props> {
+  constructor(props: any) {
+    super(props)
+    this.getList()
+  }
+
+  public state = {
+
+    typelist: [],
+    typelist_bot: []
+  }
+  public getList = async () => {
+   
+    const { getExamType } = this.props.subject
+    const { getQuestion } = this.props.question
+    
+
+
+    getExamType()
+    getQuestion()
+  
+    const typeresult = await this.props.subject.getExamType()
+    const resulttype = await this.props.question.getQuestion()
+    console.log(typeresult, resulttype)
+    this.setState({
+    
+      typelist: typeresult,
+      typelist_bot: resulttype
+    })
+  }
     public render() {
       return (
         <div className="box">
